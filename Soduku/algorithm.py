941226be1b9e4e6f432521d629cc672945cbad15
Chaitanya Swaroop Bhogapurapu <chaitanyaswaroop00@gmail.com>
2023-10-26 09:43:15 +0530

    Update algorithm.py

--- Soduku/algorithm.py
+++ Soduku/algorithm.py
@@ -58,13 +58,25 @@ def print_all(board):
 
 
 
-input_board = []
+'''input_board = []
 for j in range(9):
     l = []
     for i in range(9):
         t = int(input())
         l.append(t)
-    input_board.append(l)
+    input_board.append(l)'''
+
+input_board=  [
+    [7,8,0,4,0,0,1,2,0],
+    [6,0,0,0,7,5,0,0,9],
+    [0,0,0,6,0,1,0,7,8],
+    [0,0,7,0,4,0,2,6,0],
+    [0,0,1,0,5,0,9,3,0],
+    [9,0,4,0,6,0,0,0,5],
+    [0,7,0,3,0,0,0,1,2],
+    [1,2,0,0,0,7,4,0,0],
+    [0,4,9,2,0,6,0,0,7]
+]
 
 
 
